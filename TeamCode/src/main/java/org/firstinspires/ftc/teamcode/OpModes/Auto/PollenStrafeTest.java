5bcb1996daecb26bf0b13b35a67ba0f7733f751f
Shriya Iyengar <shriyaiyengar27@gmail.com>
2026-09-20 13:31:40 -0400

    //pollen strafe test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/PollenStrafeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/PollenStrafeTest.java
@@ -6,59 +6,43 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.Components.Mecanum;
 
-@Autonomous(name="Pollen Strafe Test",group="Testing")
+@Autonomous(name="Pollen Align Test",group="Testing")
 public class PollenStrafeTest extends LinearOpMode{
     Mecanum mecanum;
     Limelight3A limelight;
-    boolean aligned=false;
+
     @Override
     public void runOpMode() throws InterruptedException{
         mecanum=new Mecanum(hardwareMap,telemetry);
         limelight=hardwareMap.get(Limelight3A.class,"limelight");
-        // green pipeline
+        // green ball
         limelight.pipelineSwitch(1);
         limelight.start();
         telemetry.addLine("Ready");
         telemetry.update();
+
         waitForStart();
+
         while(opModeIsActive()){
             LLResult result=limelight.getLatestResult();
-
             if(result!=null&&result.isValid()){
-                double area=result.getTa();
                 double tx=result.getTx();
                 telemetry.addData("Pollen","found");
                 telemetry.addData("TX",tx);
-                telemetry.addData("Area",area);
-                // close enough
-                if(area>=8){
-                    mecanum.setPower(0,0,0);
-                    telemetry.addLine("Pollen reached");
+                // ball one side, move forward
+                if(tx>1){
+                    mecanum.setPower(0,0.25,0);
+                    telemetry.addLine("Moving forward");
                 }
-                // first line up with ball
-                else if(!aligned){
-                    if(tx>4){
-                        mecanum.setPower(0,0.25,0);
-                        telemetry.addLine("Moving forward");
-                    }
-                    else if(tx<-4){
-                        mecanum.setPower(0,-0.25,0);
-                        telemetry.addLine("Moving backward");
-                    }
-                    else{
-                        // lined up, stop for a sec
-                        mecanum.setPower(0,0,0);
-                        mecanum.writeAll();
-                        telemetry.addLine("Aligned");
-                        telemetry.update();
-                        sleep(1000);
-                        aligned=true;
-                    }
+                // ball other side, move backward
+                else if(tx<-1){
+                    mecanum.setPower(0,-0.25,0);
+                    telemetry.addLine("Moving backward");
                 }
-                // now only move toward ball
+                // camera lined up with ball
                 else{
-                    mecanum.setPower(-0.25,0,0);
-                    telemetry.addLine("Moving toward pollen");
+                    mecanum.setPower(0,0,0);
+                    telemetry.addLine("aligned");
                 }
             }
             else{

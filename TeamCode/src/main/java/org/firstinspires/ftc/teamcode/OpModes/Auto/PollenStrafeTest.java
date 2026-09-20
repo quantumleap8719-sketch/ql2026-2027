e06b7a803c288502a916a92ed2faf4665bce2256
Shriya Iyengar <shriyaiyengar27@gmail.com>
2026-09-20 12:29:41 -0400

    //pollen strafe test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/PollenStrafeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/PollenStrafeTest.java
@@ -5,60 +5,59 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.Components.Mecanum;
-@Autonomous(name = "Pollen Strafe Test", group = "Testing")
-public class PollenStrafeTest extends LinearOpMode {
+@Autonomous(name="Pollen Strafe Test",group="Testing")
+public class PollenStrafeTest extends LinearOpMode{
     Mecanum mecanum;
     Limelight3A limelight;
-
     @Override
-    public void runOpMode() throws InterruptedException {
-        mecanum = new Mecanum(hardwareMap, telemetry);
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        // Pipeline 1 = green pollen
+    public void runOpMode() throws InterruptedException{
+        mecanum=new Mecanum(hardwareMap,telemetry);
+        limelight=hardwareMap.get(Limelight3A.class,"limelight");
+
+        // green pipeline
         limelight.pipelineSwitch(1);
         limelight.start();
         telemetry.addLine("Ready");
         telemetry.update();
-
         waitForStart();
-
-        while (opModeIsActive()) {
-            LLResult result = limelight.getLatestResult();
-
-            if (result != null && result.isValid()) {
-                double area = result.getTa();
-                double tx = result.getTx();
-                telemetry.addData("Pollen", "found");
-                telemetry.addData("Area", area);
-                if (Math.abs(tx) < 3.8){
+        while(opModeIsActive()){
+            LLResult result=limelight.getLatestResult();
+            if(result!=null&&result.isValid()){
+                double area=result.getTa();
+                double tx=result.getTx();
+                telemetry.addData("Pollen","found");
+                telemetry.addData("TX",tx);
+                telemetry.addData("Area",area);
+                // close enough
+                if(area>=8){
                     mecanum.setPower(0,0,0);
+                    telemetry.addLine("Pollen reached");
                 }
-                // If pollen is still far away
-                if (area < 8) {
-                    // Strafe toward the Limelight
-                    mecanum.setPower(-0.25, 0, 0);
-                    if (tx > 4) {
-                        mecanum.setPower(-.25, .25, 0);
-                    }// Pollen to left
-                    else if (tx < -4) {
-                        mecanum.setPower(-.25, -.25, 0);
-                    }
-                } else {
-                    mecanum.setPower(0, 0, 0);
-                    telemetry.addLine("pollen reached");
+                // align forward
+                else if(tx>4){
+                    mecanum.setPower(0,0.25,0);
+                    telemetry.addLine("Moving forward");
                 }
-                // Pollen is close enough
+                // align backward
+                else if(tx<-4){
+                    mecanum.setPower(0,-0.25,0);
+                    telemetry.addLine("Moving backward");
                 }
-
-            else {
-                // Don't see pollen
-                mecanum.setPower(0, 0, 0);
-                telemetry.addLine("No Pollen");
+                // lined up, go toward ball
+                else{
+                    mecanum.setPower(-0.25,0,0);
+                    telemetry.addLine("Moving toward pollen");
+                }
+            }
+            else{
+                // no ball
+                mecanum.setPower(0,0,0);
+                telemetry.addLine("No pollen");
             }
             mecanum.writeAll();
             telemetry.update();
         }
-        mecanum.setPower(0, 0, 0);
+        mecanum.setPower(0,0,0);
         mecanum.writeAll();
         limelight.stop();
     }
